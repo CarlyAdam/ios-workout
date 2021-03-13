58fd9b03993bc7f9af9891bde6bc0c4391e723ad
carlyadam <carlyadam88@gmail.com>
2021-03-13 18:47:24 -0500

    Initial Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Carlos Adan on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
